fcc61d3d450d17014dd607fc793b7505bc5bfc76
Alex Santamaria <santamariaalex146@gmail.com>
2021-04-21 01:18:23 -0500

    refactored

--- code.js
+++ code.js
@@ -4,7 +4,23 @@ function helloWorld() {
 }
 
 // sayHello
-function sayHello() {
+function sayHello(input) {
+        return "Hello, " + input + "!";
+    }
 
-}
+/*function sayHello(x) {
+    if ((typeof x === 'string' && x.trim().length > 0) || typeof x === 'number' ) {
+        return "Hello, " + x + "!";
+    }
+    return 'Hello, World!';
+}*/
 
+
+/*
+if (input === "Alex") {
+    return "Hello, Alex!";
+} else if (input === "Pat") {
+    return "Hello, Pat!"
+} else {
+    return "Hello, Jane!";
+}*/
