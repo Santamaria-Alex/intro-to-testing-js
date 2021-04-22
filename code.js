e369811c9c5f0dbf97a04e0be1e268bf344c7a70
Alex Santamaria <santamariaalex146@gmail.com>
2021-04-22 00:00:40 -0500

    ex 12

--- code.js
+++ code.js
@@ -8,6 +8,39 @@ function sayHello(input) {
         return "Hello, " + input + "!";
     }
 
+// isFive function
+function isFive(x) {
+    return x == 5;
+}
+// isEven function
+function isEven(input) {
+    return parseFloat(input) % 2 === 0;
+}
+//is Vowel
+//function isVowel(string) {
+//    if (string === "a"||"e"||"i"||"o"||"u"||"A"||"E"||"I"||"O"||"U") {
+//        return true;
+//    }
+//}
+function isVowel(string) {
+    return string === "a" || string === "e" || string === "i"|| string === "o"|| string === "u"|| string === "A"|| string === "E"|| string === "I"|| string === "O"|| string === "U";
+}
+// add function
+//function add(x,y) {
+//    if (isNaN(x) || isNaN(y)) {
+//        return NaN;
+//    } else {
+//        return parseFloat(x) + parseFloat(y);
+//    }
+//}
+function add(x, y) {
+    if(!isNaN(parseFloat(x)) && !isNaN(parseFloat(y))) {
+        return parseFloat(x) + parseFloat(y);
+    } else {
+        return NaN;
+    }
+}
+
 /*function sayHello(x) {
     if ((typeof x === 'string' && x.trim().length > 0) || typeof x === 'number' ) {
         return "Hello, " + x + "!";
